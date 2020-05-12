a0a4935b622d6ae85ba5a53fd9f5ce54aacb69e2
SteveenDomingue <steveena.dominguezb@utadeo.edu.co>
2020-05-12 07:13:31 -0500

    Listo para desarrollar

--- CapitanAmerica/settings.py
+++ CapitanAmerica/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'CapitanAmerica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'CapitanAmerica',
+        'USER': 'IngeSo',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CO'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
