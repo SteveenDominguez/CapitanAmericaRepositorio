ebed42b70f3bd40691155e6874add266df8971e4
SteveenDomingue <steveena.dominguezb@utadeo.edu.co>
2020-05-13 20:29:33 -0500

    v1

--- companeros/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CompañerosConfig(AppConfig):
-    name = 'compaÑeros'
--- companeros/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from companeros.views import ListarCompaneros, InsertarCompaneros, EditarCompaneros, EliminarCompaneros
-
-urlpatterns=[
-    path('companeros/list',ListarCompaneros.as_view(),name="listar_companeros"),
-    path('companeros/new',InsertarCompaneros.as_view(),name="insertar_companero"),
-    path('companeros/edit<int:pk>',EditarCompaneros.as_view(),name="editar_companero"),
-    path('companeros/delete<int:pk>',EliminarCompaneros.as_view(),name="eliminar_companero"),
-]
